c7c81f34dbad621440febd92d151612baed9d7f0
tobeky <tobylewkowicz@bennington.edu>
2016-03-03 15:29:12 -0500

    working on profile app
    yep i totally forgot to sync this! my bad, will improve in future!

---
+++ TobyProfile/TobyProfile/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TobyProfile
+//
+//  Created by Toby Lewkowicz on 2/29/16.
+//  Copyright © 2016 Toby Lewkowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var profileText: UILabel!
+    @IBOutlet var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func excitingButton(sender: AnyObject) {
+        nameLabel.text = "Pokemon Trainer by night!"
+        profileText.text = "Sylveon, go!"
+        profileImage.image = UIImage(named: "Sylveon")
+    }
+
+}
+
